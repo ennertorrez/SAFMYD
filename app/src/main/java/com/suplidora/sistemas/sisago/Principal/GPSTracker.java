4c69bdc78e190911edd57c44d53062f5fb777c76
Carlos <sistemas.suplidora@hotmail.com>
2018-08-25 11:27:59 -0600

    1. se ha incrementado la frecuencia de ingreso de informacion de localizacion a 1 minuto y una distancia de 50 metros.

--- app/src/main/java/com/suplidora/sistemas/sisago/Principal/GPSTracker.java
+++ app/src/main/java/com/suplidora/sistemas/sisago/Principal/GPSTracker.java
@@ -42,8 +42,8 @@ public class GPSTracker extends Service implements LocationListener {
     String jsonCoord;
     boolean isOnline;
     boolean guardadoOK;
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 50; // 30 meters
-    private static final long MIN_TIME_BW_UPDATES = 1000 * 60; // 1/2 minute
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 50; // 50 metros
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60; // 1 minuto
     protected LocationManager locationManager;
     String IMEI;
     private AlertDialog alert = null;
